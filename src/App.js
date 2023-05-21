97c6ac0c9792a5d6c9469d3bb1a129bc22d5dc58
elale1984 <edward.gratefuldead1@gmail.com>
2023-05-21 14:59:59 -0700

    created base app for music react app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import Card from "./card";
 import './App.css';
+import ReactDOM from "react-dom";
+import albums from './Album.json';
+import SearchForm from "./SearchForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = (props) => {
+    const [searchPhrase, setSearchPhrase] = useState('');
+    const [albumList, setAlbumList] = useState([]);
+    const updateSearchResults = (phrase) => {
+        console.log('phrase is ', phrase);
+        setSearchPhrase(phrase);
+    }
+    useEffect(() => {
+        setAlbumList(albums);
+    },[albumList]);
+    const renderedList = () => {
+
+        return albumList.map((album) => {
+            if (album.description.toLowerCase().includes(searchPhrase.toLowerCase()) || searchPhrase === '')
+            return (
+                <Card
+                    key = {album.id}
+                    albumTitle={album.title}
+                    albumDescription={album.description}
+                    buttonText='OK'
+                    imgURL={album.image}/>);
+            else
+                console.log('Does not match ' + searchPhrase);
+        });
+    };
+    return <div><div className='container'><SearchForm onSubmit={updateSearchResults}/></div> <div className='container'>{renderedList()}</div></div>;
 }
-
 export default App;
